be7a3b0ce950e2f53fea7ccc61c95bf67d5b946d
Joshua S Buhl <24720475+JoshuaBuhl@users.noreply.github.com>
2022-08-30 22:14:53 +0200

    added format string for output
    Now the result will display two digits after the decimal point even if it is .x0 or .00!

--- tip_calculator.py
+++ tip_calculator.py
@@ -5,5 +5,5 @@ print("Welcome to my tip calculator!")
 original_bill = float(input("What was the total bill?\n$ "))
 tip_percentage = int(input("What percentage would you like to tip?\n% "))
 number_of_friends = int(input("How many people will split the bill?\n\U0001F91D "))
-final_split = original_bill*(tip_percentage/100+1)/number_of_friends
-print(f"Each person should pay: ${round(final_split, 2)}")
+final_split = round(original_bill*(tip_percentage/100+1)/number_of_friends, 2)
+print("Each person should pay: $ " + "{:.2f}".format(final_split))
