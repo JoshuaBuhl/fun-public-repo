2f7aac0d6ceae65e160585f48e2d566094802806
Joshua S Buhl <24720475+JoshuaBuhl@users.noreply.github.com>
2022-08-30 21:59:27 +0200

    tip_calculator.py
    Another tiny 'project' from the python course.
    This time with a fancy Emoji \U0001F91D !

---
+++ tip_calculator.py
@@ -0,0 +1,9 @@
+#tip calculator
+#this tool help you and your friends to easily tip a specific amount and split the bill evenly
+#even if the wine menu got the better of you
+print("Welcome to my tip calculator!")
+original_bill = float(input("What was the total bill?\n$ "))
+tip_percentage = int(input("What percentage would you like to tip?\n% "))
+number_of_friends = int(input("How many people will split the bill?\n\U0001F91D "))
+final_split = original_bill*(tip_percentage/100+1)/number_of_friends
+print(f"Each person should pay: ${round(final_split, 2)}")
