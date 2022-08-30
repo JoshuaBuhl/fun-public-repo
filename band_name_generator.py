1aafcfcabfb9773328e52d918595b05ccf932ad7
Joshua S Buhl <24720475+JoshuaBuhl@users.noreply.github.com>
2022-08-30 20:50:01 +0200

    band_name_generator.py
    One first little 'project' from the 100 days of code python bootcamp.

---
+++ band_name_generator.py
@@ -0,0 +1,8 @@
+# band name generator
+print("Welcome to my band name generator")
+
+city = input("Which city do you live in?\n")
+
+pet = input("What is an animal you like?\n")
+
+print("Your band name may be " + city + " " + pet + "!")
