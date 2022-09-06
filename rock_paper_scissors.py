a52cb4783bb54e0b81f29a568e82d66c0874fbf2
Joshua S Buhl <24720475+JoshuaBuhl@users.noreply.github.com>
2022-09-07 01:52:48 +0200

    added an if-statement to catch unexpected user input

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -29,16 +29,19 @@ scissors = '''
 '''
 choices = [rock, paper, scissors]
 player_choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors.\n"))
-print(choices[player_choice])
+if player_choice in range(3):
+    print(choices[player_choice])
 
-com_choice = random.randint(0, 2)
-print("Computer chose:")
-print(choices[com_choice])
+    com_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(choices[com_choice])
 
-#making use of the fact that 'choices' has it's Elements ordered in a way that Element x beats Elements x - 1.
-if com_choice == (player_choice + 1) % 3:
-    print("You lose")
-elif player_choice == com_choice:
-    print("You tie")
+    #making use of the fact that 'choices' has it's Elements ordered in a way that Element x beats Elements x - 1.
+    if com_choice == (player_choice + 1) % 3:
+        print("You lose")
+    elif player_choice == com_choice:
+        print("You tie")
+    else:
+        print("You win")
 else:
-    print("You win")
+    print("Please enter only '0', '1' or '2' to make your choice!")
