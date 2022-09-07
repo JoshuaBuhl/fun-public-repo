2cc9476fd43da598c0a0b41e7a6c4f92969c4c3c
Joshua S Buhl <24720475+JoshuaBuhl@users.noreply.github.com>
2022-09-07 10:05:20 +0200

    nr_letters now is number of alphabetical characters
    In my first implementation I misunderstood it as total number of characters of the password.

--- password_generator.py
+++ password_generator.py
@@ -9,8 +9,8 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-available_chars = [nr_letters-nr_numbers-nr_symbols]
-for number in [nr_numbers, nr_symbols]:
+available_chars = []
+for number in [nr_letters, nr_numbers, nr_symbols]:
     if number > 0:
         available_chars.append(number)
 
