3abb9987071398e303543308bfaeca4a4ae07213
Joshua S Buhl <24720475+JoshuaBuhl@users.noreply.github.com>
2022-09-07 15:59:59 +0200

    Step 3 - ability to make multiple guesses
    I'm continuing the list comprehension version now in an extra branch.

--- hangman.py
+++ hangman.py
@@ -1,32 +1,32 @@
-# Step 2
+# Step 3
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
 
 chosen_word = random.choice(word_list)
+
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-# T0D0-1: - Create an empty List called display.
-# For each letter in the chosen_word, add a "_" to 'display'.
-# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+# Create blanks
 # display = []
 # for element in range(len(chosen_word)):
 #     display.append('_')
+display = ["_" for x in chosen_word]
 
-guess = input("What is your guess?\n").lower()
+# T0D0-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+while '_' in display:
 
-# T0D0-2: - Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-# for index in range(len(chosen_word)):
-#     if chosen_word[index] == guess:
-#         display[index] = guess
+    guess = input("What is your guess?\n").lower()
 
-display = [guess if character == guess else '_' for _, character in enumerate(chosen_word)]
+    # Check guessed letter
+    # for index in range(len(chosen_word)):
+    #     if chosen_word[index] == guess:
+    #         display[index] = guess
 
-# T0D0-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-# Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+    display = [guess if character == guess else display[index] for index, character in enumerate(chosen_word)]
 
+    print(display)
+else:
+    print("Congratulations, you have won!")
